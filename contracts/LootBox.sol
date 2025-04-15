5d3d650d0628a98b65d516db0b277893569efc3c
adeel.javed <adeel.javed@vividly.it>
2025-04-15 15:45:03 +0500

    Abstract Updates

--- contracts/LootBox.sol
+++ contracts/LootBox.sol
@@ -7,17 +7,15 @@ import {MerkleProof} from "./Utils.sol";
 import {IERC1155Receiver} from "./Utils.sol";
 
 contract LootBox is ERC1155, Ownable {
-    uint256 public maxSupply = 5000;
-    uint256 public totalMinted;
     uint256 private _entropyNonce;
 
     uint256[] public tokenIdPool = [1, 2, 3, 4, 5, 6, 7];
-    uint256[] public tokenProbabilities = [32, 32, 15, 10, 6, 4, 1];
+    uint256[] public tokenProbabilities = [32, 32, 15, 10, 4, 6, 1];
 
     enum MintType {
-        Public,
         WL1,
-        WL2
+        WL2,
+        WL3
     }
 
     struct MintConfig {
@@ -46,6 +44,7 @@ contract LootBox is ERC1155, Ownable {
         bytes32 merkleRoot,
         uint8 freeMints
     );
+    event OddsUpdated(uint256[] newProbabilities);
 
     uint256 private constant _NOT_ENTERED = 1;
     uint256 private constant _ENTERED = 2;
@@ -101,18 +100,27 @@ contract LootBox is ERC1155, Ownable {
         emit URIUpdated(newURI);
     }
 
-    function setMaxSupply(uint256 newSupply) external onlyOwner {
-        require(newSupply >= totalMinted, "New supply < already minted");
-        maxSupply = newSupply;
-    }
-
     function withdraw() external onlyOwner {
         (bool success, ) = owner().call{value: address(this).balance}("");
         require(success, "Transfer failed.");
     }
 
-    function mintPublic(uint256 quantity) external payable nonReentrant {
-        _mintTokens(quantity, MintType.Public, new bytes32[](0));
+    function updateOdds(
+        uint256[] calldata newProbabilities
+    ) external onlyOwner {
+        uint256 len = newProbabilities.length;
+        require(len == tokenProbabilities.length, "Length mismatch");
+
+        uint256 sum;
+        for (uint256 i = 0; i < len; ) {
+            sum += newProbabilities[i];
+            unchecked {
+                ++i;
+            }
+        }
+        require(sum == 100, "Probabilities must sum to 100");
+        tokenProbabilities = newProbabilities;
+        emit OddsUpdated(newProbabilities);
     }
 
     function mintWL1(
@@ -129,13 +137,19 @@ contract LootBox is ERC1155, Ownable {
         _mintTokens(quantity, MintType.WL2, proof);
     }
 
+    function mintWL3(
+        uint256 quantity,
+        bytes32[] calldata proof
+    ) external payable nonReentrant {
+        _mintTokens(quantity, MintType.WL3, proof);
+    }
+
     function _mintTokens(
         uint256 quantity,
         MintType mintType,
         bytes32[] memory proof
     ) internal nonReentrant {
         require(quantity > 0, "Must mint at least 1 NFT");
-        require(totalMinted + quantity <= maxSupply, "Max supply reached");
 
         MintConfig memory config = mintConfigs[mintType];
         require(
@@ -143,56 +157,48 @@ contract LootBox is ERC1155, Ownable {
                 block.timestamp <= config.endTime,
             "Mint not active"
         );
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(
+            MerkleProof.verify(proof, config.merkleRoot, leaf),
+            "Not whitelisted"
+        );
 
-        uint256 paidQuantity = quantity;
-        uint256 freeQuantity = 0;
+        // Free vs paid calculation
+        uint256 freeMintsUsed = userMints[msg.sender][mintType];
+        uint256 freeRemaining = config.freeMintsAllowed > freeMintsUsed
+            ? config.freeMintsAllowed - freeMintsUsed
+            : 0;
+        uint256 freeQuantity = quantity > freeRemaining
+            ? freeRemaining
+            : quantity;
+        uint256 paidQuantity = quantity - freeQuantity;
 
-        if (mintType != MintType.Public) {
-            bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-            require(
-                MerkleProof.verify(proof, config.merkleRoot, leaf),
-                "Not whitelisted"
-            );
-
-            uint256 freeMintsUsed = userMints[msg.sender][mintType];
-            if (freeMintsUsed < config.freeMintsAllowed) {
-                uint256 freeRemaining = config.freeMintsAllowed - freeMintsUsed;
-                freeQuantity = quantity > freeRemaining
-                    ? freeRemaining
-                    : quantity;
-                paidQuantity = quantity - freeQuantity;
-                // Only update free mints count
-                userMints[msg.sender][mintType] += freeQuantity;
-            }
+        if (freeQuantity > 0) {
+            userMints[msg.sender][mintType] += freeQuantity;
         }
 
         require(msg.value == paidQuantity * config.price, "Incorrect ETH sent");
-
         for (uint256 i = 0; i < quantity; i++) {
             uint256 tokenId = _getRandomTokenId();
             tokenIdSupply[tokenId]++;
-            totalMinted++;
             emit Minted(msg.sender, tokenId, mintType);
-            _mint(msg.sender, tokenId, 1, ""); // Empty data prevents callbacks
+            _mint(msg.sender, tokenId, 1, "");
         }
     }
 
     function _getRandomTokenId() internal returns (uint256) {
         // Increment nonce for each generation
         _entropyNonce++;
-
         uint256 rand = uint256(
-            keccak256(
+             keccak256(
                 abi.encodePacked(
                     msg.sender,
                     tx.gasprice,
                     block.timestamp,
-                    blockhash(block.number),
-                    totalMinted
+                    blockhash(block.number)
                 )
             )
         ) % 100;
-
         uint256 cumulative;
         for (uint256 i = 0; i < tokenProbabilities.length; i++) {
             cumulative += tokenProbabilities[i];
